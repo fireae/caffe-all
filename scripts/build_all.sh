4948efa197a923e9932f0eb584a3731db26c24fe
wencc <fireae@163.com>
2016-09-22 14:43:57 +0800

    modify some .gitignore

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -4,16 +4,19 @@ set -e
 ROOT_DIR=$(readlink -f "`dirname $0`"/..)
 cd ${ROOT_DIR}/scripts
 
-./build_boost.sh
+# optional
 #./build_eigen.sh
 #./build_gflags.sh
 #./build_glog.sh
 #./build_hdf5.sh
 #./build_snappy.sh
 #./build_leveldb.sh
-./build_lmdb.sh
-./build_openblas.sh
-./build_opencv.sh
+
+# required 
+#./build_boost.sh
+#./build_lmdb.sh
+#./build_openblas.sh
 ./build_protobuf.sh
+./build_opencv.sh
 #./build_protobuf_host.sh
 ./build_caffe.sh
